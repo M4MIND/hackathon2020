38bf5eefa603b4e6a09edfebbca6d1004e0a9148
Ivan Shnurchenko <ivan.s@miro.com>
2020-12-02 13:14:10 +0500

    Added API call to take a preview

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,30 @@
-var express = require('express');
-var app = express();
+const puppeteer = require('puppeteer');
+const express = require('express');
+const app = express();
 
-app.get('/', function(req, res) {
-	res.send('hello world');
-});
+(async () => {
+	const browser = await puppeteer.launch();
+	app.get('/api/make-preview', async (req, res) => {
+		const page = await browser.newPage();
+		const opened = await page.goto('http://ya.ru'); // TODO: replace with /api/render/
+		if (399 < opened.status()) {
+			return res.status(500).send("can't load page");
+		}
+		const buf = await page.screenshot({
+			encoding: "binary",
+			type: "png",
+		})
+		const closed = page.close()
 
-app.listen(3000);
+		return res.set("Content-Type", "image/png").send(buf);
+	});
+
+	app.get('/api/render', function (req, res) {
+		// render React component
+		res.send('hello world');
+	});
+
+	app.listen(3000, () => {
+		console.log("API on localhost:3000");
+	});
+})();
