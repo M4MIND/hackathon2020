9d1ca7665dfc300f75f892082078b0f535f8b2d4
Egor Sysoev <m4mindscape@gmail.com>
2020-12-03 17:29:26 +0500

    Include PhotoPicker

--- new/pages/banners/vacancy/index.js
+++ new/pages/banners/vacancy/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+import path from "path";
+import querystring from "querystring";
+import PhotoPicker from "../../../libs/photoPicker/photo-picker"
 
 const CardsList = (props) => {
-	const {location, job, image, bg} = props;
+	const {location, job, bg, team, photo} = props;
 
 	return (
 		<div className="container" style={{backgroundColor: `#${bg}`}}>
@@ -14,20 +17,32 @@ const CardsList = (props) => {
 				</div>
 			</div>
 			<div className="container-column">
-				<div className="sharing-image-container" style={{backgroundImage: `url(/banners/vacancy/${image})`}}>
-
-				</div>
+				<div className="sharing-image-container" style={{backgroundImage: `url(/banners/vacancy/${team}/${photo})`}}></div>
 			</div>
 
 			<div className="logo-container">
-				<img src={`/banners/vacancy/miro-logo.svg`} />
+				<img src={`/banners/vacancy/miro-logo.svg`}/>
 			</div>
 		</div>
 	);
 };
 
-CardsList.getInitialProps = async (ctx) => {
-	return ctx.query||{};
+export async function getServerSideProps(ctx) {
+	const team = ctx.query.team || 'null';
+	const photoDir = path.resolve(process.cwd(), 'public', 'banners', 'vacancy', team);
+	const photoPicker = new PhotoPicker(`${photoDir}/*.png`);
+
+	const photo = await photoPicker.pick(querystring.stringify(ctx.query));
+
+	return {
+		props: {
+			location: ctx.query.location || 'location',
+			job: ctx.query.job || 'job',
+			bg: ctx.query.bg || 'FDF1C6',
+			team: ctx.query.team || 'team',
+			photo: photo || 'photo'
+		}
+	};
 }
 
 export default CardsList;
